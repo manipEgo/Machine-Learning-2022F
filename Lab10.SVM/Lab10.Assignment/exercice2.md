12237c2ca029ae6462fecf0f4b5545e07789dedb
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-11-23 02:15:28 +0800

    homework: lab10 assignment

---
+++ Lab10.SVM/Lab10.Assignment/exercice2.md
@@ -0,0 +1,19 @@
+### Exercise 2 Questions(4 points)
+
+1. Can SVM be used for unsupervised clustering or data dimension reduction? Why?
+
+    `No. SVM need at least some of the data to be labeled. Hence is supervised.`
+
+2. What are the strengths of SVMs; when do they perform well?
+
+    `It is suitable for both large and small size of data and features. It is away from the probabilities and statics. It depends on small number of features, and is hence robust.`
+
+    `It performs well on small datasets and complex decision boarders.`
+
+3. What are the weaknesses of SVMs; when do they perform poorly?
+
+    `It's efficiency declines vastly on large datasets. It depends largely on preprocessing and data quality. It is relatively hard to explain.`
+
+4. What makes SVMs a good candidate for the classification / regression problem, if you have enough knowledge about the data?
+
+    `If the dataset size is not very large and the data has many complex features, it is a good choice to apply SVMs to it.`
--- Lab10.SVM/Lab10.Assignment/experiment.py
+++ Lab10.SVM/Lab10.Assignment/experiment.py
@@ -2,13 +2,16 @@ import cv2
 from train import processFiles, trainSVM
 from detector import Detector
 
+import warnings
+warnings.filterwarnings("ignore")
+
 # Replace these with the directories containing your
 # positive and negative sample images, respectively.
 pos_dir = "samples/vehicles"
 neg_dir = "samples/non-vehicles"
 
 # Replace this with the path to your test video file.
-video_file = "videos/test_video.mp4"
+video_file = "videos/project_video.mp4"
 
 
 def experiment1():
@@ -19,7 +22,14 @@ def experiment1():
     # TODO: You need to adjust hyperparameters
     # Extract HOG features from images in the sample directories and 
     # return results and parameters in a dict.
-    feature_data = processFiles(pos_dir, neg_dir, recurse=True,hog_features=True)
+    feature_data = processFiles(pos_dir=pos_dir, neg_dir=neg_dir, recurse=True,
+                output_file=False, output_filename="result_video",
+                color_space="bgr", channels=[0, 1, 2],
+                hog_features=True, hist_features=True, spatial_features=True,
+                hog_lib="sk", size=(48, 48), hog_bins=8, pix_per_cell=(8, 8),
+                cells_per_block=(2, 2), block_stride=None, block_norm="L2",
+                transform_sqrt=True, signed_gradient=False, hist_bins=8,
+                spatial_size=(8, 8))
 
 
     # Train SVM and return the classifier and parameters in a dict.
@@ -31,7 +41,11 @@ def experiment1():
     #       to obtain a better performance
 
     # Instantiate a Detector object and load the dict from trainSVM().
-    detector = Detector().loadClassifier(classifier_data=classifier_data)
+    detector = Detector(
+        init_size=(64, 64),
+        x_overlap=0.24, y_step=0.007,
+        x_range=(0.5, 0.93), y_range=(0.55, 0.85),
+        scale=1.2).loadClassifier(classifier_data=classifier_data)
   
     # Open a VideoCapture object for the video file.
     cap = cv2.VideoCapture(video_file)
@@ -39,7 +53,11 @@ def experiment1():
     # Start the detector by supplying it with the VideoCapture object.
     # At this point, the video will be displayed, with bounding boxes
     # drawn around detected objects per the method detailed in README.md.
-    detector.detectVideo(video_capture=cap)
+    detector.detectVideo(video_capture=cap,
+                num_frames=4, threshold=47,
+                min_bbox=(32, 32),
+                show_video=True, draw_heatmap=True, draw_heatmap_size=0.3,
+                write=True, write_fps=24)
 
 
 # def experiment2
